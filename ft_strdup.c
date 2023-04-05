3a5f485d1062d14d090049d3873f73c0fa219f32
Julien Silance <jusilanc@shi-r6-p2.s19.be>
2023-04-05 19:14:28 +0200

    full fix

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:44:50 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/04/05 19:03:32 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/04/05 19:11:56 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ char	*ft_strdup(char *str)
 {
 	char	*ptr;
 
-	if (str)
-		return (NULL);
 	ptr = malloc(sizeof(char) * (ft_strlen((char *)str) + 1));
 	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, (char *)str, ft_strlen((char *)str));
+	if (str)
+		ft_memcpy(ptr, (char *)str, ft_strlen((char *)str));
 	ptr[ft_strlen((char *)str)] = 0;
 	return (ptr);
 }
